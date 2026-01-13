e5d121d5499199c96adf8fd2644999e334ac69a7
Felipe Matsuguma <feliperibeiromt@hotmail.com>
2026-01-13 18:19:16 -0300

    Exercícios feitos da aula 141

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,6 @@ public class Main
 {
 	static void main(String[] args)
 	{
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		double salario = sc.nextDouble();
-		double imposto = 0.0;
-		
-		if(salario > 4500.00)
-		{
-		
-		}
-		
-		sc.close();
+	
 	}
 }
--- src/exercises/Main.java
+++ src/exercises/Main.java
@@ -1,9 +1,25 @@
 package exercises;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main
 {
 	static void main(String[] args)
 	{
-	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+
+		for(int x = 2; x < 10000; x++)
+		{
+			if(x % n == 2)
+			{
+				System.out.println(x);
+			}
+		}
+		
+		sc.close();
 	}
 }
